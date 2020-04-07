4a060790d24250f55983cc88f060382ded35b09a
CalumWhite <dummy@email>
2020-04-07 16:55:52 +0100

    modifier and saver added

--- get_config.py
+++ get_config.py
@@ -5,6 +5,11 @@ class config_getter:
     def __init__(self):
         self.config_name = self.get_file_name()
 
+    def save_file(self, data, path=None):
+        if path == None:
+            path = self.config_name
+        vdf.dump(data, open(path, "w"), pretty=True)
+
     def get_file_name(self):
         return "/home/calum/.steam/debian-installation/userdata/89980527/config/localconfig.vdf"
 
